96e83b181b4e50e5d038dcf20db5821c68b7fee7
Mukul <mukulkolpe45@gmail.com>
2021-07-28 03:35:25 +0530

    Features updated

--- src/App.js
+++ src/App.js
@@ -1,60 +1,72 @@
 import { Component } from "react";
 import "./App.css";
 
-const WhoIsBig=(First, Second)=>{
-  if(First === Second){
-    return "Draw";
+const WhoIsBig = (First, Second) => {
+  if (First === Second) {
+    return "Tie";
   }
-switch(First){
-  case "Rock":
-    if(Second ==="Scissor")  return First;
-    else return Second;
-
+  switch (First) {
+    case "Rock":
+      if (Second === "Scissors") {
+        return First;
+      } else {
+        return Second;
+      }
     case "Paper":
-    if(Second ==="Rock")  return First;
-    else return Second;
-
-    case "Scissor":
-    if(Second ==="Paper")  return First;
-    else return Second;
-
+      if (Second === "Rock") {
+        return First;
+      } else {
+        return Second;
+      }
+    case "Scissors":
+      if (Second === "Paper") {
+        return First;
+      } else {
+        return Second;
+      }
     default:
-    
-}
-}
+  }
+};
 
 class App extends Component {
   state = {
-    started: false,
+    Started: false,
     Player: null,
     Computer: null,
+    Name: ""
   };
-  
   render() {
-    const { started, Player, Computer } = this.state;
+    const { Started, Player, Computer, Name } = this.state;
     const Images = {
       Rock: "https://i.imgur.com/TONXH9s.png",
       Paper: "https://i.imgur.com/t2154qR.png",
-      Scissor: "https://i.imgur.com/SXstPKk.png",
+      Scissors: "https://i.imgur.com/SXstPKk.png"
     };
     return (
       <div className="App">
-        <h1>Rock Paper Scissor</h1>
-        {started ? (
+        <h1>Rock Paper Scissors</h1>
+        {Started ? (
           <div className="Game">
-            <div className={"Player" + (Player ? "selected" : "")}>
+            <div className={"Player" + (Player ? " selected" : "")}>
               <p>Player</p>
               {Player ? (
-               <img src={Images[Player]} alt={Player} />
+                <img src={Images[Player]} alt={Player} />
               ) : (
                 <div className="choose">
                   {Object.keys(Images).map((a) => (
-                    <span key={a} onClick={()=>{
-                      this.setState({
-                        Player: a,
-                        Computer: Object.keys(Images)[Math.floor(Math.random() * Object.keys(Images).length)]
-                      })
-                    }}>
+                    <span
+                      key={a}
+                      onClick={() => {
+                        this.setState({
+                          Player: a,
+                          Computer: Object.keys(Images)[
+                            Math.floor(
+                              Math.random() * Object.keys(Images).length
+                            )
+                          ]
+                        });
+                      }}
+                    >
                       <img src={Images[a]} alt={a} />
                       {a}
                     </span>
@@ -64,26 +76,64 @@ class App extends Component {
             </div>
             <div className="Computer">
               <p>Computer</p>
-              {Computer ?  
-              <img src={Images[Computer]} alt={Computer} /> :
-              <img src="https://i.imgur.com/CyvHqQH.png" alt="All Choices" />}
-              
+              {Computer ? (
+                <img src={Images[Computer]} alt={Computer} />
+              ) : (
+                <img src="https://i.imgur.com/CyvHqQH.png" alt="All Choices" />
+              )}
             </div>
           </div>
         ) : (
-          <img
-            className="start"
-            src="https://i.imgur.com/FrnyhhB.png"
-            alt="start"
-            onClick={() => {
-              this.setState({
-                started: true,
-              });
-            }}
-          />
+          <div className="Intro">
+            <input
+              type="text"
+              placeholder="Enter your name, at least 3 characters long..."
+              value={Name}
+              onChange={(e) => {
+                this.setState({ Name: e.target.value });
+              }}
+            />
+            {Name.trim().length > 2 && (
+              <img
+                className="start"
+                src="https://i.imgur.com/FrnyhhB.png"
+                alt="Start"
+                onClick={() => {
+                  this.setState({
+                    Started: true
+                  });
+                }}
+              />
+            )}
+          </div>
         )}
-        {Player && Computer && <p>{WhoIsBig(Player, Computer)} Wins!</p>}
-        
+        {Player && Computer && (
+          <p className="Results">
+            {(() => {
+              const Winner = WhoIsBig(Player, Computer);
+              if (Winner === "Tie") {
+                return "Nobody Wins!";
+              } else {
+                if (Winner === Player) {
+                  return Name + " Wins!";
+                } else {
+                  return "Computer Wins!";
+                }
+              }
+            })()}
+            <img
+              src="https://i.imgur.com/529CybI.png"
+              alt="Restart"
+              onClick={() => {
+                this.setState({
+                  Started: false,
+                  Player: null,
+                  Computer: null
+                });
+              }}
+            />
+          </p>
+        )}
       </div>
     );
   }
